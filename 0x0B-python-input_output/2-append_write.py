544856cecf182229aa695abd51e95a90a6f5d4da
Evans-Kipkemoi <evanskipkemoi7@gmail.com>
2022-09-27 14:27:52 -0700

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-'''  function that reads n lines of a text file (UTF8) and prints it to stdout
-'''
+"""
+function that appends a string at the end of a text file (UTF8) and returns the number of characters added
+"""
 
-
-def read_lines(filename="", nb_lines=0):
-    ''' function: read_lines
-    '''
-    if filename == "" or type(filename) != str:
-        return
-    if type(nb_lines) != int:
-        return
-    counter = 0
-    with open(filename, "r") as f:
-        for line in f:
-            counter += 1
-            if nb_lines <= 0 or (counter <= nb_lines and nb_lines > 0):
-                print(line, end='')
-            else:
-                break
+def append_write(filename="", text=""):
+    with open(filename, 'a') as f:
+        return f.write(text)
